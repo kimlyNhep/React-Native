1716371d769dff953de737e218746827a2d3148a
kimly Nhep <kimly.nhep0168@gmail.com>
2019-08-19 16:36:15 +0700

    done

--- SmartStart/App.js
+++ SmartStart/App.js
@@ -1,49 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-
 import HomeScreen from "./src/HomeScreen";
 
 const navigator = createStackNavigator({
-  DrawNav: {
-    screen: HomeScreen,
-    defaultNavigationOptions: ({ navigation }) => ({
-      //Hide the shadow of the header
-      headerStyle: {
-        elevation: 0,
-        shadowColor: "transparent",
-        shadowRadius: 0,
-        shadowOffsest: {
-          height: 0
-        }
-      },
-      headerLeft: (
-        <View>
-          <Button
-            onPress={() => {
-              navigation.toggleDrawer();
-            }}
-            title="Info"
-            color="#2220"
-          />
-        </View>
-      )
-    })
+  {
+    Home: HomeScreen
+  },
+  {
+    : HomeScreen,
+    defaultNavigationOptions: {
+      title: "Exchange"
+    }
   }
 });
-
-const navigationOptionsHeader = ({ navigation }) => {
-  return {
-    headerRight: (
-      <Button
-        onPress={() => {
-          navigation.toggleDrawer();
-        }}
-        title="Info"
-        color="#2220"
-      />
-    )
-  };
-};
 
 export default createAppContainer(navigator);
